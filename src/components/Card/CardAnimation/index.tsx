b78f0d380ed349a363f23868e977926fdbd43dbe
Iann-rst <iannrst11@gmail.com>
2022-06-29 19:17:39 -0300

    feat: animação opacidade e posição no eixo X

--- src/components/Card/CardAnimation/index.tsx
+++ src/components/Card/CardAnimation/index.tsx
@@ -1,9 +1,12 @@
 import React, { useEffect } from 'react';
 import { useWindowDimensions, ViewProps } from 'react-native';
 import {
+  interpolate,
   useAnimatedStyle,
   useSharedValue,
-  withTiming
+  Extrapolate,
+  withTiming,
+  runOnJS
 } from 'react-native-reanimated';
 
 import { AnimationContainer } from './styles';
@@ -20,6 +23,12 @@ export function CardAnimation({ children, ...rest }: CardAnimationProps) {
   const animatedStyle = useAnimatedStyle(() => {
     return {
       // TODO - setup animated style
+      opacity: interpolate(cardOpacity.value, [0, 1], [0, 1], Extrapolate.CLAMP),
+      transform: [
+        {
+          translateX: cardOffset.value
+        }
+      ]
     }
   })
 
@@ -28,6 +37,14 @@ export function CardAnimation({ children, ...rest }: CardAnimationProps) {
      * TODO - setup cardOpacity.value and cardOffset.value with
      * withTiming()
      */
+    cardOpacity.value = withTiming(
+      1,
+      { duration: 1000 }
+    );
+    cardOffset.value = withTiming(
+      0,
+      { duration: 1000 }
+    )
   }, []);
 
   return (
@@ -36,3 +53,15 @@ export function CardAnimation({ children, ...rest }: CardAnimationProps) {
     </AnimationContainer>
   )
 }
+
+/**
+ *  ALGUMAS INFORMAÇÕES IMPORTANTES
+ * withTiming: muda o valor de uma propriedade ao longo de uma duração
+ *  cardOffset.value = withTiming(0, {duration: 1000}):
+ *    Muda o valor de cardOffset para 0 durante 1segundo
+ * 
+ * interpolate: calcular o valor que tem que ser colocado durante o intervalo do percurso 
+ * opacity: interpolate(cardOpacity.value, [0, 1], [0, 1])
+ *    Mudar o valor da opacidade durante o intervalo de 0 a 1, calculando o valor que a opacidade vai ter durante
+ *    o percurso da animação
+ */
